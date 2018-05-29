8526db30890d94a51a9669c766786ea4ef7ea5ba
Tianchi Xu <xtc459924276@gmail.com>
2018-05-29 15:19:46 -0400

    Add and style GitHub component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 import { Route, Switch, NavLink } from 'react-router-dom'
+
+import './App.css'
+import Github from './Github'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h3>Ain't no party like this</h3>
-          <h1>API party</h1>
+          <h3>Ain't no party like an</h3>
+          <h1>API Party</h1>
         </div>
-        <ul>
+        <ul className="navLinks">
           <li>
-            <NavLink to='/github'>github api</NavLink>
+            <NavLink to="/github">GitHub API</NavLink>
           </li>
         </ul>
         <Switch>
-          <Route path='/github' render={() => <h1>github</h1>}></Route>
-          <Route render={() => <p>Click some link</p>}></Route>
+          <Route path="/github" component={Github} />
+          <Route render={() => <p>To get started, click one of the links above.</p>} />
         </Switch>
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
