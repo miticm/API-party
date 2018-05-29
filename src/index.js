0f8d24c7964834ddae12c3a4235df2a7de5793be
Tianchi Xu <xtc459924276@gmail.com>
2018-05-29 15:27:37 -0400

    Add submit handler that pushes username onto history

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
